b8ca6ff80cb04ec7f5c532a61b34b146389b7e14
Enes Arda <enesarda@Eness-MacBook-Air-4.local>
2021-03-21 15:53:17 +0300

    change sorting

--- app.js
+++ app.js
@@ -104,47 +104,128 @@ app.get("/index", function(req, res) {
   if (searchedName) {
     const regex = new RegExp(escapeRegex(searchedName), 'gi');
     const regex2 = new RegExp(escapeRegex(req.query.search), 'gi');
-    Instructor.find({
-      name: regex
-    }, function(err, foundInstructors) {
-      if (err) {
-        console.log(err);
-      } else {
-        Course.find({
-          fullName: regex2
-        }, function(err, foundCourses) {
+
+    Instructor.countDocuments({name: regex}, function(err, instructorCount) {
+      Course.countDocuments({fullName: regex2}, function(err2, courseCount) {
+
+        const initialPage = Math.ceil(instructorCount/15);
+
+        Instructor.find({name: regex}).sort({name:1}).skip(15*(page-1)).limit(15).exec(function(err, foundInstructors) {
           if (err) {
             console.log(err);
           } else {
-            res.render("index", {
-              instructors: foundInstructors,
-              courses: foundCourses,
-              page: page,
-              search: req.query.search
-            })
+            const initialAmount = 15-foundInstructors.length;
+            if(foundInstructors.length===15) {
+              res.render("index", {
+                items: [foundInstructors],
+                page: page,
+                numberOfPages: Math.ceil((instructorCount+courseCount)/15),
+                search: req.query.search
+              });
+            } else {
+
+              if(page===initialPage) {
+
+                Course.find({fullName: regex2}).sort({fullName: 1}).skip(0).limit(initialAmount).exec(function(err, foundCourses) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    res.render("index", {
+                      items: [foundInstructors, foundCourses],
+                      page: page,
+                      numberOfPages: Math.ceil((instructorCount+courseCount)/15),
+                      search: req.query.search
+                    });
+                  }
+                });
+
+              } else {
+
+                Course.find({fullName: regex2}).sort({fullName: 1}).skip(initialAmount + 15*(page-initialPage-1)).limit(15).exec(function(err, foundCourses) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    res.render("index", {
+                      items: [foundInstructors, foundCourses],
+                      page: page,
+                      numberOfPages: Math.ceil((instructorCount+courseCount)/15),
+                      search: req.query.search
+                    });
+                  }
+                });
+
+              }
+
+
+            }
           }
         });
-      }
+
+
+      });
     });
   } else {
-    Instructor.find({}, function(err, foundInstructors) {
-      if (err) {
-        console.log(err);
-      } else {
-        Course.find({}, function(err, foundCourses) {
+
+    Instructor.countDocuments({}, function(err, instructorCount) {
+      Course.countDocuments({}, function(err2, courseCount) {
+
+        const initialPage = Math.ceil(instructorCount/15);
+
+        Instructor.find({}).sort({name:1}).skip(15*(page-1)).limit(15).exec(function(err, foundInstructors) {
           if (err) {
             console.log(err);
           } else {
-            res.render("index", {
-              instructors: foundInstructors,
-              courses: foundCourses,
-              page: page,
-              search: req.query.search
-            })
+            const initialAmount = 15-foundInstructors.length;
+            if(foundInstructors.length===15) {
+              res.render("index", {
+                items: [foundInstructors],
+                page: page,
+                numberOfPages: Math.ceil((instructorCount+courseCount)/15),
+                search: req.query.search
+              });
+            } else {
+
+              if(page===initialPage) {
+
+                Course.find({}).sort({fullName: 1}).skip(0).limit(initialAmount).exec(function(err, foundCourses) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    res.render("index", {
+                      items: [foundInstructors, foundCourses],
+                      page: page,
+                      numberOfPages: Math.ceil((instructorCount+courseCount)/15),
+                      search: req.query.search
+                    });
+                  }
+                });
+
+              } else {
+
+                Course.find({}).sort({fullName: 1}).skip(initialAmount + 15*(page-initialPage-1)).limit(15).exec(function(err, foundCourses) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    res.render("index", {
+                      items: [foundInstructors, foundCourses],
+                      page: page,
+                      numberOfPages: Math.ceil((instructorCount+courseCount)/15),
+                      search: req.query.search
+                    });
+                  }
+                });
+
+              }
+
+            }
           }
         });
-      }
+
+
+      });
     });
+
+
   }
 });
 
@@ -228,40 +309,6 @@ app.post("/rate", function(req, res) {
     },
     attendance: (req.body.attendance === '1')
   }
-
-  // Instructor.findOneAndUpdate({
-  //   name: req.body.instructor
-  // }, {
-  //   $push: {
-  //     ratings: newRating
-  //   }
-  // }, function(err, foundInstructor) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     const lecturingAvg = (foundInstructor.lecturingAvg * (foundInstructor.ratings.length + 1) + newRating.lecturingValue) / (foundInstructor.ratings.length + 2);
-  //     const studentRelAvg = (foundInstructor.studentRelAvg * (foundInstructor.ratings.length + 1) + newRating.studentRelValue) / (foundInstructor.ratings.length + 2);
-  //     const difficultyAvg = (foundInstructor.difficultyAvg * (foundInstructor.ratings.length + 1) + newRating.difficultyValue) / (foundInstructor.ratings.length + 2);
-  //     const gradingAvg = (foundInstructor.gradingAvg * (foundInstructor.ratings.length + 1) + newRating.gradingValue) / (foundInstructor.ratings.length + 2);
-  //     const overallAvg = (lecturingAvg + studentRelAvg + gradingAvg) / 3
-  //
-  //     foundInstructor.lecturingAvg = parseInt(lecturingAvg);
-  //     foundInstructor.studentRelAvg = parseInt(studentRelAvg);
-  //     foundInstructor.difficultyAvg = parseInt(difficultyAvg);
-  //     foundInstructor.gradingAvg = parseInt(gradingAvg);
-  //     foundInstructor.overallAvg = parseInt(overallAvg);
-  //     foundInstructor.save(function(err) {
-  //       if (err) {
-  //         console.log(err);
-  //         req.flash('error', 'değerlendirmeniz eklenirken sorun oluştu');
-  //         res.redirect("/instructors/" + foundInstructor.name);
-  //       } else {
-  //         req.flash('success', 'değerlendirmeniz başarılı bir şekilde eklendi');
-  //         res.redirect("/instructors/" + foundInstructor.name);
-  //       }
-  //     });
-  //   }
-  // });
 
   Instructor.findOne({
     name: req.body.instructor
